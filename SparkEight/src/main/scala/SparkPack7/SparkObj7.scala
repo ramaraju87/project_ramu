e6e746c5944c76570274f7c297846f484aee3992
ramaraju87 <vramaraju87@gmail.com>
2021-06-20 15:11:15 +0530

    commited

---
+++ SparkEight/src/main/scala/SparkPack7/SparkObj7.scala
@@ -0,0 +1,17 @@
+package SparkPack7
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import sys.process._
+
+
+object SparkObj7 {
+  def main(args:Array[String]):Unit={
+    
+			val conf = new SparkConf().setAppName("ES").setMaster("local[*]")
+					val sc = new SparkContext(conf)
+					sc.setLogLevel("ERROR")
+  
+}
+  
+}
